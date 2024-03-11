bb86d5dfa5866bb1590e51043d07ec47cfc10b7d
Lingouda Patil <136546601+lingoudappatil@users.noreply.github.com>
2024-03-11 02:56:20 +0000

    container configure done

---
+++ JavaScript_Basics/variables.js
@@ -0,0 +1,42 @@
+let num1 = 2
+let num2 = 6
+let num3 = 4
+function but()
+{
+if(num1==num2)
+{
+    console.log("num1 is Equal to num2");
+}else
+	if(num1> num2)
+{
+    console.log("num1 is Greater than num2");
+}else
+{
+    console.log("num1 is Lesser than num2");
+}
+}
+but()
+console.log("<br><br><br>");
+function floop()
+{
+for( let num = 0; num<=10; num++)
+{
+	console.log("\n"+ num);
+}
+console.log("<br><br><br><br>");
+}
+floop()
+
+var abc = 1;
+	while(abc <= 5)
+	{
+		console.log(abc);
+		abc++;
+	}
+	console.log("<br><br><br><br>");
+	let bca=5;
+	do{
+		console.log(bca);
+		bca--;
+	}
+	while(bca>= 0)
