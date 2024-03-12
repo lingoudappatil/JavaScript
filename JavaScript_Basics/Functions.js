ff4c2e4e59c4b50466c40dcda3e1060a31603d0a
Lingouda Patil <136546601+lingoudappatil@users.noreply.github.com>
2024-03-12 17:15:45 +0000

    container configure done

---
+++ JavaScript_Basics/Functions.js
@@ -0,0 +1,16 @@
+function arr() {
+    var MyArr = new Array("JS", "JSON", "JQ");
+    console.log("This is your result " + MyArr.length);
+  }
+  arr()
+  //********* */
+  function Regular()
+   {
+    var Myarr2 = [];
+    Myarr2[0] = "JS";
+    Myarr2[1] = "JSON";
+    Myarr2[2] = "JQ";
+    Myarr2[3] = "AJX";
+    console.log("My Array elememts are \n" + Myarr2);
+  }
+  Regular()
