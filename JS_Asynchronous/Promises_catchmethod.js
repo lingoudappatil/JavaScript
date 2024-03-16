53e74a2dee146910a1ef905dc31c769609c81dde
Lingouda Patil <136546601+lingoudappatil@users.noreply.github.com>
2024-03-16 14:38:54 +0000

    CONTAINER CONFIGURE DONE

--- JS_Asynchronous/Promises_catchmethod.js
+++ JS_Asynchronous/Promises_catchmethod.js
@@ -1,7 +1,7 @@
 // Example using catch() method in Promises
 const getPromise = () => {
     return new Promise((resolve, reject) => {
-        console.log("I am Promise");
+        console.log("I am ASHOK Promise");
         reject("Error occurred");
     });
 };
