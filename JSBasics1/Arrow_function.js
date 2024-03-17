d1f84c7896d061aaa34fb68d9b6d3d3489b3616a
Lingouda Patil <136546601+lingoudappatil@users.noreply.github.com>
2024-03-17 13:13:54 +0000

    lingu

---
+++ JSBasics1/Arrow_function.js
@@ -0,0 +1,17 @@
+// Traditional anonymous function
+console.log((function(a) {
+    return a + 1;
+})(10)); 
+
+
+// Arrow functions
+console.log((a => {
+    return a + 2;
+})(10)); 
+
+console.log((a => a + 3)(10)); 
+
+console.log(((a) => a + 4)(10)); 
+
+// Single parameter doesn't need parentheses
+console.log((a => a + 5)(10)); 
---
+++ JSBasics1/class_static_method.js
@@ -0,0 +1,24 @@
+class Point {
+    constructor(x, y) {
+      this.x = x;
+      this.y = y;
+    }
+  
+    static displayName = "Point";
+    static distance(a, b) {
+      const dx = a.x - b.x;
+      const dy = a.y - b.y;
+  
+      return Math.hypot(dx, dy);
+    }
+  }
+  
+  const p1 = new Point(5, 5);
+  const p2 = new Point(10, 10);
+  p1.displayName;
+  p1.distance; 
+  p2.displayName; 
+  p2.distance;
+  
+  console.log(Point.displayName);
+  console.log(Point.distance(p1, p2));
