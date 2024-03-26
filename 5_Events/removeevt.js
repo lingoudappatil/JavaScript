79356e963e5888136320a4a76c97181537fd4e38
Lingouda Patil <136546601+lingoudappatil@users.noreply.github.com>
2024-03-26 12:28:38 +0000

    evt

---
+++ 5_Events/removeevt.js
@@ -0,0 +1,14 @@
+let btn5 = document.querySelector("#btn5");
+btn5.addEventListener("click",(evt) =>{
+    console.log("The button1 clicked - handling 1");
+});
+btn5.addEventListener("click",(evt) =>{
+    console.log("The button1 clicked - handling 2");
+});
+const hand2=() =>{
+    console.log("The button1 clicked - handling 3");
+});
+btn5.remove
+btn5.addEventListener("click",(evt) =>{
+    console.log("The button1 clicked - handling 4");
+});
