654f5e7aae17b363c39c87e6cb7e15236fbeae00
Lingouda Patil <136546601+lingoudappatil@users.noreply.github.com>
2024-05-03 10:06:34 +0000

    rn

---
+++ 5_API/api3.js
@@ -0,0 +1,10 @@
+const URL = "https://cat-fact.herokuapp.com/facts";
+
+const getFacts = async () =>
+{
+	console.log("Getting data from API...");
+	let response = await fetch(URL);
+	console.log(response);//JSON Formate
+	let data = await response.json();
+    console.log(data[0].text);
+};	
