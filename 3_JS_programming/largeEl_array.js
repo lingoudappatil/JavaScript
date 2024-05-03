40c784c8fd421f84fc00491e7da33637a0bf1b62
Lingouda Patil <136546601+lingoudappatil@users.noreply.github.com>
2024-05-03 10:51:58 +0000

    ar

---
+++ 3_JS_programming/largeEl_array.js
@@ -0,0 +1,14 @@
+var array = [2,9,8,3,9,6];
+var largest = array[0];
+if(array.length === 0)
+{
+        return null;
+}
+ for(var i=0;i<=array.length;i++)
+{
+  if(array[i] > largest)
+  {
+    largest = array[i]; 
+  }
+}
+console.log("largest is " + largest);
