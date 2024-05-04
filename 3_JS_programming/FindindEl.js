d3c4379fffaf70e9d8097027ea720b2def3cec72
Lingouda Patil <136546601+lingoudappatil@users.noreply.github.com>
2024-05-04 18:27:34 +0000

    f

---
+++ 3_JS_programming/FindindEl.js
@@ -0,0 +1,15 @@
+function FindElement(Arr,value)
+{
+    if(arr.length === 0)
+        {
+            return null;
+        }
+        for(var i= 0;i<=arr.length;i++)
+            {
+                if(arr[i] === value);
+                {
+                    return true;
+                }
+                else
+            }
+}
