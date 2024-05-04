904a68606f69d32d5d4030b45adda1fdc9c533fa
Lingouda Patil <136546601+lingoudappatil@users.noreply.github.com>
2024-05-04 18:23:15 +0000

    evn

---
+++ 3_JS_programming/Arr_Position.js
@@ -0,0 +1,11 @@
+var ar= [2,4,5,9,7,1,0];
+var EvenAr =[];
+for(var i=0;i<=ar.length;i++)
+{
+    if(i%2 ===0)
+    {
+                console.log("Even Numbers are below\n");
+                EvenAr.push(ar[i]);
+    }
+}console.log("Array is" +EvenAr);
+return EvenAr;
