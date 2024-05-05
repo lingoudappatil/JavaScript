42bb507ef71a291e25b23c09e24ae6275642741b
Lingouda Patil <136546601+lingoudappatil@users.noreply.github.com>
2024-05-05 14:21:20 +0000

    printing words with string

---
+++ 3_JS_programming/Num_Word.js
@@ -0,0 +1,20 @@
+var n=15;
+for(var i=0;i <= n; i++)
+{
+    if(i%3 ===0 && i%5 === 0 )
+        {
+            console.log("FiggBuzz");
+        }
+     else if(i%3 === 0)
+        {
+                    console.log("Figg");
+        }
+     else if(i%5 === 0 )
+        {
+        console.log("Buzz");
+        }
+     else
+        {
+        console.log(i);
+     }
+}
