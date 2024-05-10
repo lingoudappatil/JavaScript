1979b7fad9abeecdaafba6507b74834b9b67e7bd
Lingouda Patil <136546601+lingoudappatil@users.noreply.github.com>
2024-05-10 18:06:14 +0000

    str

---
+++ 3_JS_programming/string/Cap.js
@@ -0,0 +1,6 @@
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+// Example usage:
+console.log(capitalizeFirstLetter("hello")); // Outputs: Hello
---
+++ 3_JS_programming/string/swap.js
@@ -0,0 +1,9 @@
+let a = 5;
+let b = 10;
+
+a = a + b; // a = 15, b = 10
+b = a - b; // a = 15, b = 5
+a = a - b; // a = 10, b = 5
+
+console.log("a =", a); // Output: 10
+console.log("b =", b); // Output: 5
