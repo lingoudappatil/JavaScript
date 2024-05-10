4642b7645502b9e5b47b6a65dd4fcff42ab28b9d
Lingouda Patil <136546601+lingoudappatil@users.noreply.github.com>
2024-05-10 18:12:51 +0000

    swap

--- 3_JS_programming/string/swap.js
+++ 3_JS_programming/string/swap.js
@@ -1,7 +1,7 @@
 let a = 5;
 let b = 10;
-a = a + b; // a = 15, b = 10
-b = a - b; // a = 15, b = 5
+a = a + b; 
+b = a - b; 
 a = a - b; // a = 10, b = 5
 console.log("a =", a); 
 console.log("b =", b); 
