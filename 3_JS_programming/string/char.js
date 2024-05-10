7ae6182547a24a9de33ae576cea73b2fad65e1b7
Lingouda Patil <136546601+lingoudappatil@users.noreply.github.com>
2024-05-10 18:11:28 +0000

    char

--- 3_JS_programming/string/char.js
+++ 3_JS_programming/string/char.js
@@ -1,4 +1,3 @@
 let str = "Hello, World!";
 let totalCharacters = str.length;
-
 console.log("Total number of characters:", totalCharacters); // Output: 13
--- 3_JS_programming/string/swap.js
+++ 3_JS_programming/string/swap.js
@@ -1,9 +1,7 @@
 let a = 5;
 let b = 10;
-
 a = a + b; // a = 15, b = 10
 b = a - b; // a = 15, b = 5
 a = a - b; // a = 10, b = 5
-
-console.log("a =", a); // Output: 10
-console.log("b =", b); // Output: 5
+console.log("a =", a); 
+console.log("b =", b); 
