16760ae05954d9b84ef269f78cd2ac704f8d5c40
Lingouda Patil <136546601+lingoudappatil@users.noreply.github.com>
2024-07-08 23:30:48 +0530

    Finding Element from Array

--- 3_JS_programming/Arrays/FindingEl.js
+++ 3_JS_programming/Arrays/FindingEl.js
@@ -1,7 +1,7 @@
 function FindElement(arr,value)
 {
         for(var i= 0;i<arr.length;i++)
-            {
+            { 
                 if(arr[i] === value)
                 {
                     console.log("true");
