8ab21bfc20377f6cf49599c63715f16342c00472
Lingouda Patil <136546601+lingoudappatil@users.noreply.github.com>
2024-07-19 18:17:11 +0530

    evtlistener.js

--- 6_Events/evtlistener.js
+++ 6_Events/evtlistener.js
@@ -1,6 +1,6 @@
 let btn3 = document.querySelector("#btn3");
 btn3.addEventListener("click",(evt) =>
-{
+{ 
     console.log("The button1 clicked - handling 1");
 });
 btn3.addEventListener("click",(evt) =>
