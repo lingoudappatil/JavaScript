b28bfb241da7d7fe1eea869e39300ec050943344
Lingouda Patil <136546601+lingoudappatil@users.noreply.github.com>
2024-07-22 22:36:18 +0530

    Array.js

--- 1_JSBasics/Array.js
+++ 1_JSBasics/Array.js
@@ -1,3 +1,3 @@
-//Simple Example on Array 
+//Simple Example on Array  
 var MyArr=new Array("JS","JSON","JQ");
 console.log("Click on button to disply the number of elements in Array",+MyArr.length);
