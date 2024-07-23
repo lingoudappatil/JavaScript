39bf0462d02304fee59a5cc1fa5e1266e049261c
Lingouda Patil <136546601+lingoudappatil@users.noreply.github.com>
2024-07-23 22:34:21 +0530

    Promises_simple_exp.js

--- 4_JS_Asynchronous/Promises_simple_exp.js
+++ 4_JS_Asynchronous/Promises_simple_exp.js
@@ -1,7 +1,7 @@
 //Promises --> It is for eventual completion of task.It is an Object in JS
 
 let promise=new Promise((resolve,reject) =>
-{
+{ 
     console.log("I am a Promises");
     resolve("Success");
 });
